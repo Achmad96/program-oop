bc6a91947377ddb9b3c49a0e019374eeaad71e63
Achmad Raihan <totsukamail3278@gmail.com>
2024-08-20 14:47:43 +0700

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+  public static void main(String[] args) {
+    Handphone handphone = new Handphone();
+    handphone.hidupkan();
+    handphone.kirimSMS();
+    handphone.lakukanPanggilan();
+    handphone.matikan();
+
+    Mahasiswa mahasiswa = new Mahasiswa();
+    mahasiswa.membaca();
+    mahasiswa.menyontek();
+    mahasiswa.modifikasi();
+
+    Matematika matematika = new Matematika();
+    matematika.penambahan(10, 5);
+    matematika.pengurangan(10, 5);
+    matematika.perkalian(10, 5);
+    matematika.pembagian(10, 5);
+  }
+}
---
+++ src/Matematika.java
@@ -0,0 +1,17 @@
+public class Matematika {
+  public void perkalian (double a, double b) {
+    System.out.printf( "%d x %d = %d", a, b, a * b);
+  }
+
+  public void pembagian (double a, double b) {
+    System.out.printf( "%d / %d = %d", a, b, a / b);
+  }
+
+  public void penambahan (double a, double b) {
+    System.out.printf( "%d + %d = %d", a, b, a + b);
+  }
+
+  public void pengurangan (double a, double b) {
+    System.out.printf( "%d - %d = %d", a, b, a - b);
+  }
+}
